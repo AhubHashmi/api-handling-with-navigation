ddb91efdf6f82b0c3728f6e19dab0054f0a47da3
Muhammad Ahub Hashmi <107793320+AhubHashmi@users.noreply.github.com>
2022-12-06 00:26:50 +0500

    updated!

--- singleProduct.js
+++ singleProduct.js
@@ -6,7 +6,13 @@ function Product ({navigation,route}){
 
     return <>
     <View>
-        <Text>{route.params.title}</Text>
+        <Text>id: {route.params.id}</Text>
+        <Text>title: {route.params.title}</Text>
+        <Text>price: {route.params.price}</Text>
+        <Text>description: {route.params.description}</Text>
+        <Text>category: {route.params.category}</Text>
+        <Text>rating rate: {route.params.rating.rate}</Text>
+        <Text>rating count: {route.params.rating.count}</Text>
     </View>
     </>
 }
